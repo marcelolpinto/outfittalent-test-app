e88217ad2a2685ca1791048909b17b4e2f0c5a20
marcelolpinto <marcelo.lpinto90@gmail.com>
2022-08-15 13:01:36 -0300

    fixed identation

--- src/components/LaunchList/hook.tsx
+++ src/components/LaunchList/hook.tsx
@@ -2,21 +2,21 @@ import * as Apollo from '@apollo/client';
 import { QUERY_LAUNCH_LIST } from './query';
 
 export type LaunchListQuery = {
-    __typename?: 'Query',
-    launches?: Array<
-        {
-            __typename?: 'Launch',
-            details?: string | null;
-            flight_number?: number | null,
-            mission_name?: string | null,
-            launch_year?: number | null,
-            launch_date_utc?: string | null,
-            launch_success: boolean,
-            upcoming: boolean,
-        } | null
-    > | null
+	__typename?: 'Query',
+	launches?: Array<
+		{
+			__typename?: 'Launch',
+			details?: string | null;
+			flight_number?: number | null,
+			mission_name?: string | null,
+			launch_year?: number | null,
+			launch_date_utc?: string | null,
+			launch_success: boolean,
+			upcoming: boolean,
+		} | null
+	> | null
 };
 
 export const useLaunchListQuery = () => {
-    return Apollo.useQuery(QUERY_LAUNCH_LIST, {});
+	return Apollo.useQuery(QUERY_LAUNCH_LIST, {});
 }
