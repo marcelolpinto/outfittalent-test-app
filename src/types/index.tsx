b60cbdaf5dc2a3733ba3f277eecf043630126fbd
marcelolpinto <marcelo.lpinto90@gmail.com>
2022-08-15 13:06:13 -0300

    removed LaunchSite and Rocket types

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -16,26 +16,10 @@ export type Launch = {
     launch_date_local?: Maybe<string>;
     launch_date_unix?: Maybe<number>;
     launch_date_utc?: Maybe<string>;
-    launch_site?: Maybe<LaunchSite>;
     launch_success?: Maybe<boolean>;
     launch_year?: Maybe<number>;
     mission_id?: Maybe<Array<Maybe<string>>>;
     mission_name?: Maybe<string>;
-    rocket?: Maybe<LaunchRocket>;
     tbd?: Maybe<boolean>;
     upcoming?: Maybe<boolean>;
-};
-
-export type LaunchSite = {
-    __typename?: 'LaunchSite';
-    site_id?: Maybe<string>;
-    site_name?: Maybe<string>;
-    site_name_long?: Maybe<string>;
-};
-
-export type LaunchRocket = {
-    __typename?: 'LaunchRocket';
-    rocket_id?: Maybe<string>;
-    rocket_name?: Maybe<string>;
-    rocket_type?: Maybe<string>;
 };
